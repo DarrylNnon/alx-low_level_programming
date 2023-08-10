50e455016ccce004ce4004a17622aa1ba11c6475
DarrylNnon <darrylnnon@gmail.com>
2023-08-10 13:20:53 -0700

    task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main-Entry point.
+ *
+ *Description: Size is just a number,and territory does not make a nation.
+ *Return: 0 is always good at the of billion.
+ */
+int main(void)
+{
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	return (0);
+}
