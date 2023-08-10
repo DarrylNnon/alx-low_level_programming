2ef957b46ae4cbd5917c53f8d8e72aa72ab6caa1
DarrylNnon <darrylnnon@gmail.com>
2023-08-10 12:39:10 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  *main-Entry point
  *
+ *Description: first day coding
  *Return: Always 0 (Success)
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
- 
