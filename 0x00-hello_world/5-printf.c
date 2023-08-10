61f38dfa136fea2142d7e842ac2bcc4da0a601a7
DarrylNnon <darrylnnon@gmail.com>
2023-08-10 12:55:04 -0700

    task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  *main - Entry point
- * 
- *Description:first coding session
+ *
+ *Description: first day coding session
  *Return: Always 0 (Success)
  */
 int main(void)
-{ 
-	printf("with proper grammar, but the outcome is a piece of art,\n");	
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
