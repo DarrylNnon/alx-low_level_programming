81b6493be5a570a4756f4006f89626746d1855fc
DarrylNnon <darrylnnon@gmail.com>
2023-08-11 12:32:26 -0700

    task 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char g;
+
+	for (g = 'a' ; g <= 'z' ; g++)
+		putchar(m);
+	putchar("\n");
+	return (0);
+}
