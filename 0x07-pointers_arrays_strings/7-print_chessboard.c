32792c595dafc1eb052132bdf76b2973b599f8c2
DarrylNnon <darrylnnon@gmail.com>
2023-08-28 04:28:40 -0700

    task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,1 +1,23 @@
-return later
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: input pointer.
+ * Return: no return.
+ * Author: Darryl Nnon
+ */
+void print_chessboard(char (*a)[8])
+{
+	unsigned int b, m = 0;
+
+	for (b = 0; b < 64; b++)
+	{
+		if (b % 8 == 0 && b != 0)
+		{
+			m = b;
+			_putchar('\n');
+		}
+		_putchar(a[b / 8][b - m]);
+	}
+	_putchar('\n');
+}
