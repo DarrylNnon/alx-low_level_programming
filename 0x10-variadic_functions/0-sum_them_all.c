ae4013a92fa605a9766e89b8a206bab389b76c2c
DarrylNnon <darrylnnon@gmail.com>
2023-09-14 02:36:56 -0700

    Beauty is variable

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,23 +1,23 @@
 #include "variadic_functions.h"
+
 /**
  * sum_them_all - returns the sum of all its parameters.
  * @n: amount of the arguments.
  * Author: Darryl Nnon
  * Return: sum of its parameters.
  */
-
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list valist;
-	unsigned int j;
+	unsigned int b;
 	int sum = 0;
+
 	if (n == 0)
-	
 	return (0);
 
 	va_start(valist, n);
 
-	for (j = 0; j < n; j++)
+	for (b = 0; b < n; b++)
 	sum += va_arg(valist, int);
 
 	va_end(valist);
