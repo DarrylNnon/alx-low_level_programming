387573b7ebeccb1a70818eb2bad1071e43de3dfb
DarrylNnon <darrylnnon@gmail.com>
2023-09-21 00:36:02 -0700

    singly project

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * list_len - determine all the number of the elements in a list.
+ * @h: singly linked list.
+ * Return: all number of elements in the list.
+ * Author: Darryl Nnon
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t nelem;
+
+	nelem = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		nelem++;
+	}
+	return (nelem);
+}
